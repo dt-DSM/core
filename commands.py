702ef1f7baf1a3af3b8e4c282ab29047a74571a7
driftbluestone <driftbluestone@gmail.com>
2026-08-29 12:13:38 -0700

    upgrade command registry creation

--- commands.py
+++ commands.py
@@ -1,20 +1,48 @@
-# from registry import registry
-from driftech_lib import jsonIO, db
+# imports must go BELOW command declarations!
 
-async def tls(start: int = None, end: int = None):
+def tls(start: int = None, end: int = None):
     TLSs = db.sql.SQL("SELECT tls FROM {schema}.server_types").format(schema=db.SCHEMA)
     result = db.multiple(TLSs)
     return jsonIO.dumpb([x[0] for x in result][start:end])
 
-# async def st(tls, start: int = None, end: int = None):
-#     if manager := registry.managers.get(tls):
-#         if sts := registry.server_types[manager].get(tls):
-#             return sts[start:end]
-#         else:
-#             return None
-#     else:
-#         return None
+def st(tls, start: int = None, end: int = None):
+    result = db.get("server_types", (tls,), ("tls",), ("st",))
+    return jsonIO.dumpb(result[start:end])
 
-command_registry = {
-    "tls": tls
-}
+def create(tls: str, st: str, name: str):
+    pass
+
+def response(id: str, args: dict):
+    pass
+
+def start(server: str):
+    pass
+
+def stop(server: str):
+    pass
+
+def configs(server: str):
+    pass
+
+def set(server: str, config: str, value):
+    pass
+
+def status(server: str):
+    pass
+
+def cmd(server: str, command: str):
+    pass
+
+def msg(server: str, text: str):
+    pass
+
+# registry creation
+cmds = dir()
+import sys
+from driftech_lib import jsonIO, db
+command_registry = {}
+module = sys.modules[__name__]
+for command in cmds:
+    if command.startswith("_"):
+        continue
+    command_registry[cmd] = getattr(module, cmd)
